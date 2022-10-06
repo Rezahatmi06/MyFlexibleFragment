9ac23f8a340e732f10740daed410b3c6cea46f09
Lenovo <rezaahatmi24@gmaiul.com>
2022-10-06 15:32:52 +0700

    CategoryFragment

--- app/src/main/java/com/example/myflexiblefragment/HomeFragment.kt
+++ app/src/main/java/com/example/myflexiblefragment/HomeFragment.kt
@@ -21,6 +21,16 @@ class HomeFragment : Fragment(), View.OnClickListener {
         btnCategory.setOnClickListener(this)
     }
     override fun onClick(v: View) {
+        if (v.id == R.id.btn_category) {
+            val mCategoryFragment = CategoryFragment()
+            val mFragmentManager = parentFragmentManager
+            mFragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, mCategoryFragment, CategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
 
     }
 }
